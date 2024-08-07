6ad00236273690b036435b21386accf633c26526
LucasEspLau <lukaespinoza12@gmail.com>
2024-08-07 10:44:18 -0500

    correcion de html y vite

--- microfront-2/vite.config.ts
+++ microfront-2/vite.config.ts
@@ -5,16 +5,4 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        microfront2: 'src/main.tsx', // Punto de entrada para el microfrontend
-      },
-      output: {
-        entryFileNames: 'microfront-2.js', // Nombre del archivo JS
-        format: 'es', // Módulo ES
-      },
-    },
-  },
 });
